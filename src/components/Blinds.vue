7bf8cbdbfec0fd81c82d76cb5adb112ba0f35e78
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-03-26 03:34:28 +0100

    fixed bug related to modal content

--- src/components/Blinds.vue
+++ src/components/Blinds.vue
@@ -1,38 +1,38 @@
 <template>
     <div class="card" style="width: 18rem;">
-        <img :src="blind.images.main" class="card-img-top" :alt="blind.name">
-        <div class="card-body text-center">
-            <h5 class="card-title">{{ blind.name }}</h5>
-            <p class="card-text">From {{ blind.price_per_metre_squared }}</p>
-            <button href="#" class="btn btn-primary d-block" data-bs-toggle="modal" 
-            data-bs-target="#exampleModal" 
-            @click="showModal = true">Get Price</button>
-        </div>
+      <img :src="blind.images.main" class="card-img-top" :alt="blind.name">
+      <div class="card-body text-center">
+        <h5 class="card-title">{{ blind.name }}</h5>
+        <p class="card-text">From {{ blind.price_per_metre_squared }}</p>
+        <button href="#" class="btn btn-primary d-block" data-bs-toggle="modal" :data-bs-target="'#exampleModal-' + blindId" @click="showModal = true">Get Price</button>
+      </div>
+      <Modal :blind="blind" :show="showModal" :modalId="'exampleModal-' + blindId" @close="showModal = false"></Modal>
     </div>
-    <Modal :blind="blind" :show="showModal" @close="showModal = false"></Modal>
-</template>
+  </template>
   
-<script>
-
-    import Modal from './Modal.vue'
-    export default {
-        name: 'Blind',
-        props: {
-            blind: Object
-        },
-        components: {
-            Modal
-        },
-        data() {
-        return {
-        showModal: false,
-        };
+  <script>
+  import Modal from './Modal.vue';
+  
+  export default {
+    name: 'Blind',
+    props: {
+      blind: Object,
+      blindId: Number
     },
-        }
-</script>
-
-<style scoped>
-    button{
-        width: 100% !important;
+    components: {
+      Modal
+    },
+    data() {
+      return {
+        showModal: false,
+      };
     }
-</style>
+  };
+  </script>
+  
+  <style scoped>
+    button {
+      width: 100% !important;
+    }
+  </style>
+  
--- src/components/BlindsList.vue
+++ src/components/BlindsList.vue
@@ -1,24 +1,20 @@
 <template>
-    <div class="container mb-5 mx-auto row justify-content-between">
-      <div class="mt-5 col-sm-6 col-md-6 col-lg-3" v-for="blind in blindsList" :key="blind.id">
-        <Blinds :blind="blind"/>
-      </div>
+  <div class="container mb-5 mx-auto row justify-content-between">
+    <div class="mt-5 col-sm-6 col-md-6 col-lg-3" v-for="(blind, index) in blindsList" :key="index">
+      <Blinds :blind="blind" :blindId="'blind-' + index"/>
     </div>
-  </template>  
+  </div>
+</template>
 
 <script>
 import Blinds from './Blinds.vue';
-    export default {
-        name: 'BlindsList',
-        props: {
-        blindsList: Array,
-    },
-        components: {
-            Blinds,
-        }
-    }
+export default {
+  name: 'BlindsList',
+  props: {
+    blindsList: Array,
+  },
+  components: {
+    Blinds,
+  }
+}
 </script>
-
-<style scope>
-    
-</style>
